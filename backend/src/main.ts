c8122f94108b41490bb77a167e51acfdced1004f
Wilson Kinyua <wilsonkinyuam@gmail.com>
2024-08-21 21:57:47 +0300

    feat: Added global validation pipe & exception filter for better API error handling

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpRequestExceptionFilter } from './common/filters/http-request-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe()); // global validation pipe
+  app.useGlobalFilters(new HttpRequestExceptionFilter()); // global exception filter
+  await app.listen(3000); 
 }
 bootstrap();
