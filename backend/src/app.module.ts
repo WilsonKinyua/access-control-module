1af578f262bcb72cc6e0b0e5f5d52cbca63d6452
Wilson Kinyua <wilsonkinyuam@gmail.com>
2024-08-21 22:02:50 +0300

    chore: Updated versions of NestJS dependencies like @nestjs/common, @nestjs/config. Necessary for keeping dependencies up-to-date.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { User } from './users/entities/user.entity';
+import { Role } from './roles/entities/role.entity';
+import { Project } from './projects/entities/project.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // load the .env file and make it available in the entire application
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // connect to the database
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User, Role, Project],
+      // synchronize: true, // setting to true to auto create tables (dev only)
+    }),
+    UsersModule,
+    ProjectsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,],
 })
-export class AppModule {}
+export class AppModule { }
