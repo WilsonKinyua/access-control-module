3160ec322ade39028a9736fed28f364ff38ea4b6
Wilson Kinyua <wilsonkinyuam@gmail.com>
2024-08-23 18:58:00 +0300

    chore: Enabled CORS in app creation and added global CORS setup, necessary for cross-origin resource sharing restrictions

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from './app.module';
 import { HttpRequestExceptionFilter } from './common/filters/http-request-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors(); // enable CORS
   app.useGlobalPipes(new ValidationPipe()); // global validation pipe
   app.useGlobalFilters(new HttpRequestExceptionFilter()); // global exception filter
-  await app.listen(3000); 
+  await app.listen(3000);
 }
 bootstrap();
