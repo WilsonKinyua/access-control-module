8d6074cba2f7d8c91356ad2135b0d9ac9cdfeecf
Wilson Kinyua <wilsonkinyuam@gmail.com>
2024-08-24 01:43:30 +0300

    chore: Update port configuration for dynamic assignment. Enhances flexibility.

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   app.enableCors(); // enable CORS
   app.useGlobalPipes(new ValidationPipe()); // global validation pipe
   app.useGlobalFilters(new HttpRequestExceptionFilter()); // global exception filter
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
