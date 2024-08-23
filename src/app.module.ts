eed49298590d3102425d5e2a61bd8a68e0b61cf8
Wilson Kinyua <wilsonkinyuam@gmail.com>
2024-08-24 02:17:44 +0300

    ```text chore: Update port configuration to use DATABASE_URL
    This commit updates the port configuration in the app.module.ts file to use the DATABASE_URL environment variable instead of individual database connection parameters. This change improves flexibility and adaptability by allowing for dynamic assignment of the database connection details.

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,11 +19,12 @@ import { AuthModule } from './auth/auth.module';
     // connect to the database
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
+      // host: process.env.DB_HOST,
+      // port: +process.env.DB_PORT,
+      // username: process.env.DB_USER,
+      // password: process.env.DB_PASSWORD,
+      // database: process.env.DB_NAME,
       entities: [User, Role, Project],
       // synchronize: true, // setting to true to auto create tables (dev only)
     }),
