9efb2f3dc84bb8bc3417714cd446ba70c41b5f43
Wilson Kinyua <wilsonkinyuam@gmail.com>
2024-08-24 17:44:42 +0300

    style: Improve responsiveness in LoginView layout for larger screens, better UI.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,7 +3,7 @@ import LoginForm from "@/components/LoginForm.vue";
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center lg:h-screen">
+  <div class="lg:flex lg:flex-col items-center justify-center lg:h-screen">
     <LoginForm />
   </div>
 </template>
