32402b0417162804a8070b87643929b33dc5e9d0
Wilson Kinyua <wilsonkinyuam@gmail.com>
2024-08-24 17:45:04 +0300

    chore: Replace axios usage with axiosInstance in projects and users modules. Enhances codebase.

--- src/store/modules/projects.ts
+++ src/store/modules/projects.ts
@@ -1,102 +1,94 @@
-import axios from 'axios';
-import type { Project } from '@/types/Project';
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+import type { Project } from "@/types/Project";
+import axiosInstance from "../axiosInstance";
 
 interface ProjectsState {
-    projects: Project[];
-    project: Project | null;
+  projects: Project[];
+  project: Project | null;
 }
 
-// Axios instance
-const axiosInstance = axios.create({
-    baseURL: API_BASE_URL,
-});
-
-// request interceptor to include the access token
-axiosInstance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
-
 export const projects = {
-    namespaced: true,
-    state: {
-        projects: [],
-        project: null,
+  namespaced: true,
+  state: {
+    projects: [],
+    project: null,
+  },
+  getters: {
+    allProjects: (state: ProjectsState) => state.projects,
+    getProject: (state: ProjectsState) => state.project,
+  },
+  mutations: {
+    setProjects(state: ProjectsState, projects: Project[]) {
+      state.projects = projects;
     },
-    getters: {
-        allProjects: (state: ProjectsState) => state.projects,
-        getProject: (state: ProjectsState) => state.project,
+    setProject(state: ProjectsState, project: Project) {
+      state.project = project;
     },
-    mutations: {
-        setProjects(state: ProjectsState, projects: Project[]) {
-            state.projects = projects;
-        },
-        setProject(state: ProjectsState, project: Project) {
-            state.project = project;
-        },
-        addProject(state: ProjectsState, project: Project) {
-            state.projects.push(project);
-        },
-        updateProject(state: ProjectsState, updatedProject: Project) {
-            const index = state.projects.findIndex(project => project.id === updatedProject.id);
-            if (index !== -1) {
-                state.projects.splice(index, 1, updatedProject);
-            }
-        },
-        removeProject(state: ProjectsState, projectId: number) {
-            state.projects = state.projects.filter(project => project.id !== projectId);
-        },
+    addProject(state: ProjectsState, project: Project) {
+      state.projects.push(project);
     },
-    actions: {
-        async fetchProjects({ commit }: { commit: Function }) {
-            try {
-                const response = await axiosInstance.get('/projects');
-                commit('setProjects', response.data);
-            } catch (error: any) {
-                throw new Error(error.response.data.message);
-            }
-        },
-        async fetchProject({ commit }: { commit: Function }, projectId: number) {
-            try {
-                const response = await axiosInstance.get(`/projects/${projectId}`);
-                commit('setProject', response.data);
-            } catch (error: any) {
-                throw new Error(error.response.data.message);
-            }
-        },
-        async createProject({ commit }: { commit: Function }, projectData: Project) {
-            try {
-                const response = await axiosInstance.post('/projects', projectData);
-                commit('addProject', response.data);
-            } catch (error: any) {
-                throw new Error(error.response.data.message);
-            }
-        },
-        async updateProject({ commit }: { commit: Function }, { projectId, projectData }: { projectId: number, projectData: Project }) {
-            try {
-                const response = await axiosInstance.patch(`/projects/${projectId}`, projectData);
-                commit('updateProject', response.data);
-            } catch (error: any) {
-                throw new Error(error.response.data.message);
-            }
-        },
-        async deleteProject({ commit }: { commit: Function }, projectId: number) {
-            try {
-                await axiosInstance.delete(`/projects/${projectId}`);
-                commit('removeProject', projectId);
-            } catch (error: any) {
-                throw new Error(error.response.data.message);
-            }
-        },
+    updateProject(state: ProjectsState, updatedProject: Project) {
+      const index = state.projects.findIndex(
+        (project) => project.id === updatedProject.id
+      );
+      if (index !== -1) {
+        state.projects.splice(index, 1, updatedProject);
+      }
     },
+    removeProject(state: ProjectsState, projectId: number) {
+      state.projects = state.projects.filter(
+        (project) => project.id !== projectId
+      );
+    },
+  },
+  actions: {
+    async fetchProjects({ commit }: { commit: Function }) {
+      try {
+        const response = await axiosInstance.get("/projects");
+        commit("setProjects", response.data);
+      } catch (error: any) {
+        throw new Error(error.response.data.message);
+      }
+    },
+    async fetchProject({ commit }: { commit: Function }, projectId: number) {
+      try {
+        const response = await axiosInstance.get(`/projects/${projectId}`);
+        commit("setProject", response.data);
+      } catch (error: any) {
+        throw new Error(error.response.data.message);
+      }
+    },
+    async createProject(
+      { commit }: { commit: Function },
+      projectData: Project
+    ) {
+      try {
+        const response = await axiosInstance.post("/projects", projectData);
+        commit("addProject", response.data);
+      } catch (error: any) {
+        throw new Error(error.response.data.message);
+      }
+    },
+    async updateProject(
+      { commit }: { commit: Function },
+      { projectId, projectData }: { projectId: number; projectData: Project }
+    ) {
+      try {
+        const response = await axiosInstance.patch(
+          `/projects/${projectId}`,
+          projectData
+        );
+        commit("updateProject", response.data);
+      } catch (error: any) {
+        throw new Error(error.response.data.message);
+      }
+    },
+    async deleteProject({ commit }: { commit: Function }, projectId: number) {
+      try {
+        await axiosInstance.delete(`/projects/${projectId}`);
+        commit("removeProject", projectId);
+      } catch (error: any) {
+        throw new Error(error.response.data.message);
+      }
+    },
+  },
 };
--- src/store/modules/users.ts
+++ src/store/modules/users.ts
@@ -1,31 +1,10 @@
-import axios from 'axios';
-import type { User } from '@/types/User';
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+import type { User } from "@/types/User";
+import axiosInstance from "../axiosInstance";
 
 interface UsersState {
   users: User[];
   user: User | null;
 }
-
-// Axios instance
-const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
-});
-
-// request interceptor to include the access token
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export const users = {
   namespaced: true,
@@ -48,20 +27,20 @@ export const users = {
       state.users.push(user);
     },
     updateUser(state: UsersState, updatedUser: User) {
-      const index = state.users.findIndex(user => user.id === updatedUser.id);
+      const index = state.users.findIndex((user) => user.id === updatedUser.id);
       if (index !== -1) {
         state.users.splice(index, 1, updatedUser);
       }
     },
     removeUser(state: UsersState, userId: number) {
-      state.users = state.users.filter(user => user.id !== userId);
+      state.users = state.users.filter((user) => user.id !== userId);
     },
   },
   actions: {
     async fetchUsers({ commit }: { commit: Function }) {
       try {
-        const response = await axiosInstance.get('/users');
-        commit('setUsers', response.data);
+        const response = await axiosInstance.get("/users");
+        commit("setUsers", response.data);
       } catch (error: any) {
         throw new Error(error.response.data.message);
       }
@@ -69,23 +48,29 @@ export const users = {
     async fetchUser({ commit }: { commit: Function }, userId: number) {
       try {
         const response = await axiosInstance.get(`/users/${userId}`);
-        commit('setUser', response.data);
+        commit("setUser", response.data);
       } catch (error: any) {
         throw new Error(error.response.data.message);
       }
     },
     async createUser({ commit }: { commit: Function }, userData: User) {
       try {
-        const response = await axiosInstance.post('/users', userData);
-        commit('addUser', response.data);
+        const response = await axiosInstance.post("/users", userData);
+        commit("addUser", response.data);
       } catch (error: any) {
         throw new Error(error.response.data.message);
       }
     },
-    async updateUser({ commit }: { commit: Function }, { userId, userData }: { userId: number, userData: User }) {
+    async updateUser(
+      { commit }: { commit: Function },
+      { userId, userData }: { userId: number; userData: User }
+    ) {
       try {
-        const response = await axiosInstance.patch(`/users/${userId}`, userData);
-        commit('updateUser', response.data);
+        const response = await axiosInstance.patch(
+          `/users/${userId}`,
+          userData
+        );
+        commit("updateUser", response.data);
       } catch (error: any) {
         throw new Error(error.response.data.message);
       }
@@ -93,7 +78,7 @@ export const users = {
     async deleteUser({ commit }: { commit: Function }, userId: number) {
       try {
         await axiosInstance.delete(`/users/${userId}`);
-        commit('removeUser', userId);
+        commit("removeUser", userId);
       } catch (error: any) {
         throw new Error(error.response.data.message);
       }
